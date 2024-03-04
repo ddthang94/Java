bd36953c145e5d3c621b70dd87f7d42564d1ea7c
ddthang94 <ddthang21194@gmail.com>
2024-03-04 19:55:20 +0700

    basic

--- lesson_1.java
+++ lesson_1.java
@@ -1,5 +1,11 @@
 public class lesson_1 {
     public static void main(String[] args) {
         System.out.println("Hello world..!");
+        System.out.println("\nnext line");
+        System.out.println("\ttab");
+        System.out.print("123 ");
+        System.out.println(" 456");
+        /* Ctrl+Shift+/ -> Note */
+        /* Ctrl+D -> copy line */
     }
 }
