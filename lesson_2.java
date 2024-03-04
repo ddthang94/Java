5674f2a8b90de24e500c281be98396a3ea03c196
ddthang94 <ddthang21194@gmail.com>
2024-03-04 22:01:58 +0700

    Datatype

---
+++ lesson_2.java
@@ -0,0 +1,20 @@
+public class lesson_2 {
+    public static void main(String[] args) {
+        System.out.println("============\"8 Primitive datatype\"=============");
+        System.out.println("boolean, char, byte, short, int, long, float, double");
+        System.out.println("Primitive datatype has a specific size and a specific range of values it can represent");
+        System.out.println("=========================");
+        System.out.println("Boolean: True or False");
+        System.out.println("Char: characters");
+        System.out.println("\n============\"Using wrapper class to check MIN, MAX value of number datatype\"=============");
+        System.out.println("Byte: 8bit from " + Byte.MIN_VALUE + " to " + Byte.MAX_VALUE);
+        System.out.println("Short: 16bit from " + Short.MIN_VALUE + " to " + Short.MAX_VALUE);
+        System.out.println("Int: 32bit from " + Integer.MIN_VALUE + " to " + Integer.MAX_VALUE);
+        System.out.println("Long: 64bit from " + Long.MIN_VALUE + " to " + Long.MAX_VALUE);
+        System.out.println("Float: 32bit from " + Float.MIN_VALUE + " to " + Float.MAX_VALUE);
+        System.out.println("Double: 64bit from " + Double.MIN_VALUE + " to " + Double.MAX_VALUE);
+        System.out.println("\n============Variable declaration=============");
+        System.out.println("\n============Variable initialization=============");
+        /* https://ioflood.com/blog/primitive-data-types-in-java/#:~:text=Java%20has%20eight%20primitive%20data,of%20values%20it%20can%20represent.&text=In%20this%20example%2C%20we've,Java's%20eight%20primitive%20data%20types. */
+    }
+}
