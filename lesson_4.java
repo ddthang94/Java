537ab92fc7676fc2462f2d1ff31ae43a81f71546
ddthang94 <ddthang21194@gmail.com>
2024-03-07 20:53:16 +0700

    operations

---
+++ lesson_4.java
@@ -0,0 +1,25 @@
+public class lesson_4 {
+    public static void main(String[] args) {
+        /* Operations in Java */
+        int a = 11;
+        int b = 5;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        /* Addition */
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+        System.out.println("Addition a + b = " + (a+b));
+        /* Subtraction */
+        int sub = a - b;
+        System.out.println("Subtraction a - b = " + sub);
+        /* Multiplication */
+        int mul = a*b;
+        System.out.println("Multiplication a * b = " + mul);
+        /* Division */
+        double div = (double) a/b;
+        System.out.println("Division a / b = " + div);
+        /* Remain */
+        int remain = a%b;
+        System.out.println("Remain = " + remain);
+    }
+}
