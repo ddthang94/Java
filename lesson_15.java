1411c358845928b42fe5a7441b0335de7e32a38a
ddthang94 <ddthang21194@gmail.com>
2024-03-14 20:56:39 +0700

    for

---
+++ lesson_15.java
@@ -0,0 +1,9 @@
+public class lesson_15 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i=0;i<=10;i+=2) {
+            sum+=i;
+            System.out.println("Sum = "+sum);
+        }
+    }
+}
