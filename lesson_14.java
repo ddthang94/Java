6ba91dfe3830a475abf54524acc716e456f40216
ddthang94 <ddthang21194@gmail.com>
2024-03-14 20:44:47 +0700

    while true

---
+++ lesson_14.java
@@ -0,0 +1,11 @@
+public class lesson_14 {
+    public static void main(String[] args) {
+        int n = 0;
+        while (true) {
+            n++;
+            System.out.println("n = " + n);
+            if (n==5)
+                break;
+        }
+    }
+}
