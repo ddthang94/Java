7f6f23ea08f97fb2a5ff961cde412b041ed2db6f
ddthang94 <ddthang21194@gmail.com>
2024-04-12 09:24:07 +0700

    streamAPI sorting

--- com/java8features/StreamAPI/v3/StreamListSorting.java
+++ com/java8features/StreamAPI/v3/StreamListSorting.java
@@ -3,7 +3,6 @@ package com.java8features.StreamAPI.v3;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class StreamListSorting {
     public static void main(String[] args) {
